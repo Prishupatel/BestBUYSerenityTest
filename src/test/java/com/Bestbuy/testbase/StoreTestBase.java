94d328d393e0dfb733939bb56a5e0b3028856c15
prishupatel <prishupatel218@gmail.com>
2022-01-11 09:23:26 -0700

    BestBuy CRUD test

---
+++ src/test/java/com/Bestbuy/testbase/StoreTestBase.java
@@ -0,0 +1,20 @@
+package com.Bestbuy.testbase;
+
+import com.Bestbuy.constants.Path;
+import com.Bestbuy.utils.PropertyReader;
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
+
+public class StoreTestBase {
+    public static PropertyReader propertyReader;
+
+    @BeforeClass
+    public static void init() {
+        propertyReader = PropertyReader.getInstance();
+        RestAssured.baseURI = propertyReader.getProperty("baseUrl");
+        RestAssured.port = Integer.parseInt(propertyReader.getProperty("port"));
+        RestAssured.basePath = Path.STORES;
+
+    }
+
+}
